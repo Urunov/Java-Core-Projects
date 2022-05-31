a3c5b651e78418cf3e628f404eb3a35d389f5759
urunov <hamdamboy.urunov@gmail.com>
2022-05-31 18:43:23 +0500

    Project API

--- API-Lesson1/src/main/java/uz/bepro/projects/model/JsonPHProject.java
+++ API-Lesson1/src/main/java/uz/bepro/projects/model/JsonPHProject.java
@@ -2,9 +2,11 @@ package uz.bepro.projects.model;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import org.apache.poi.xwpf.usermodel.*;
 
 import java.io.*;
 import java.lang.reflect.Type;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
@@ -29,11 +31,44 @@ public class JsonPHProject {
             ArrayList<Post> posts = gson.fromJson(reader, type);
 
             XWPFDocument document = new XWPFDocument();
-            FileOutputStream file = new FileOutputStream("jsonholder.docx");
+            document.createParagraph().createRun().setText("List of Post information: ");
+            XWPFTable xwpfTable = document.createTable();
+            XWPFTableRow tableRow = xwpfTable.getRow(0);
+            tableRow.getCell(0).setText("Id");
+            tableRow.addNewTableCell().setText("User Id");
+            tableRow.addNewTableCell().setText("Title");
+            tableRow.addNewTableCell().setText("Body");
 
+            for (Post post : posts) {
+                tableRow  = xwpfTable.createRow();
+//                //create paragraph
+//                XWPFParagraph paragraph = document.createParagraph();
+//
+//                //Set bottom border to paragraph
+//                paragraph.setBorderBottom(Borders.BASIC_BLACK_DOTS);
+//
+//                //Set left border to paragraph
+//                paragraph.setBorderLeft(Borders.BASIC_WIDE_INLINE);
+//
+//                //Set right border to paragraph
+//                paragraph.setBorderRight(Borders.BASIC_BLACK_DASHES);
+//
+//                //Set top border to paragraph
+//                paragraph.setBorderTop(Borders.BASIC_BLACK_DASHES);
+                tableRow.getCell(0).setText(post.getId().toString());
+                tableRow.getCell(1).setText(post.getUserId().toString());
+                tableRow.getCell(2).setText(post.getTitle());
+                tableRow.getCell(3).setText(post.getBody());
+
+            }
+
+            FileOutputStream file = new FileOutputStream("jsonholder.docx");
+            System.out.println("Process completed.");
             document.write(file);
 
-        }catch (IOException e){
+        }catch (MalformedURLException ee){
+            ee.printStackTrace();
+        } catch (IOException e){
             e.printStackTrace();
         }
     }
--- Advanced-Project-Lesson2/.idea/sonarlint/issuestore/index.pb
+++ Advanced-Project-Lesson2/.idea/sonarlint/issuestore/index.pb
@@ -1,15 +1,15 @@
 
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+^
+.src/main/java/uz/bepro/projects/FileWords.java,f/5/f51643099a21b829220a3a973c37251f1e7de44d
 b
 2src/main/java/uz/bepro/projects/FileWordTable.java,c/2/c2f3bcab9c344cb93d8b50d1cdcaf55024e7c048
 g
 7src/main/java/uz/bepro/projects/FileWordExtraction.java,0/5/056b3b2b620ba8e306564498416db9b9ede3b528
-f
-6src/main/java/uz/bepro/projects/ConnectionExample.java,2/8/28cf932c16a1381cd44171a70ecb16e8ebdd4af9
 \
 ,src/main/java/uz/bepro/projects/FilePDF.java,5/4/5412506e3d14ea845ff7160b8b8fc41c90c62424
-^
-.src/main/java/uz/bepro/projects/FileWords.java,f/5/f51643099a21b829220a3a973c37251f1e7de44d
-7
-pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+f
+6src/main/java/uz/bepro/projects/ConnectionExample.java,2/8/28cf932c16a1381cd44171a70ecb16e8ebdd4af9
 c
 3src/main/java/uz/bepro/projects/FileWordBorder.java,2/5/258440b35c470e703d67ceb7f917c2aff264bfaf
